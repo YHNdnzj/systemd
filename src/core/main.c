6a48c026c6cff8c29f20279e06f81253e1b43b02
Mike Yuan <me@yhndnzj.com>
2024-11-08 19:28:43 +0100

    fixup! tree-wide: eliminate sysv compat (sysv-generator, rc-local-generator, telinit/initctl)

--- src/core/main.c
+++ src/core/main.c
@@ -571,7 +571,7 @@ static int parse_proc_cmdline_item(const char *key, const char *value, void *dat
         } else if (!value) {
                 const char *target;
 
-                /* Compatible with SysV, but supported independently even if SysV compatibility is disabled. */
+                /* Compatible with SysV */
                 target = runlevel_to_target(key);
                 if (target)
                         return free_and_strdup_warn(&arg_default_unit, target);
