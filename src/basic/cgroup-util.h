0e029671bffd89b497d8e3fb83ca471bb6dbb1de
Mike Yuan <me@yhndnzj.com>
2025-01-15 22:05:56 +0100

    cgroup-util: drop cg_freezer_supported(), assume supported

--- src/basic/cgroup-util.h
+++ src/basic/cgroup-util.h
@@ -315,7 +315,6 @@ int cg_mask_to_string(CGroupMask mask, char **ret);
 int cg_kernel_controllers(Set **controllers);
 
 bool cg_ns_supported(void);
-bool cg_freezer_supported(void);
 bool cg_kill_supported(void);
 
 int cg_all_unified(void);
